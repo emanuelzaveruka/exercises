2468902f28bf1dbfc65edb1bea5cd70da5f95e23
Emanuel Zaveruka <emanuelzaveruka@gmail.com>
2022-04-29 15:28:58 -0300

    Encerrando exercício de NODE e EJS

--- logicExercises/rocketseat/discover/Fundamentar/node/NodeAndEJS/server.js
+++ logicExercises/rocketseat/discover/Fundamentar/node/NodeAndEJS/server.js
@@ -18,15 +18,51 @@ app.set("view engine", "ejs")
  * res = response onde pelo metodo render ()
  * Chamamos o nosso index onde o mesmo não é necessário
  * colocar .ejs o expresse já faz isso por nós
+ * Para que a pagina seja encontrada deve estar
+ * Dentro de uma pasta views 
  */
 app.get("/", function(req, res) {
-    res.render("index")
+    const items = [{
+            title: "D",
+            message: "esenvolver em ejs easy"
+        },
+        {
+            title: "E",
+            message: "manuel Zaveruka"
+        },
+        {
+            title: "M",
+            message: "anu... Isso meu apelido é manu"
+        },
+        {
+            title: "A",
+            message: "utroísta eu sou"
+        },
+        {
+            title: "I",
+            message: "ndex é padrão de muitos projetos"
+        },
+        {
+            title: "S",
+            message: "ASS"
+        }
+    ];
+    subtitle = "Uma linguagem de modelagem para criação de página HTML"
+        //Neste metódo render passamos o index
+        //E também o objeto que criamos acima
+        //O render aceita o objeto 
+    res.render("pages/index", {
+        subtitle: subtitle,
+        qualitys: items,
+    })
 })
 
-app.get("/", function(req, res) {
-        res.render("about")
-    })
-    /*
-     * 
-     */
-app.listen(8080)
+app.get("/sobre", function(req, res) {
+    res.render("pages/about")
+})
+
+/*
+ * Metodo que indica a porta que esta executando o express
+ */
+app.listen(8080);
+console.log("rodando")
